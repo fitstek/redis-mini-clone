61cf9d52b004cdbdc051a78d4b9fec720854919e
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-27 12:21:36 +0100

    echo implemented for a single command but this is not realistic as clients keep connection open and give multiple commands

--- lib/rredis/server.rb
+++ lib/rredis/server.rb
@@ -17,7 +17,28 @@ module Rredis
     end
 
     def handle_client(client)
-      client.write "+PONG\r\n"
+      # arguments sent to the redis server have a format of
+      # *<number of arguments> CR LF
+      header = client.gets.to_s
+
+      return unless header[0] == '*'
+
+      number_of_arguments = header[1..-1].to_i
+
+      # we loop number of arument times and get all values
+      cmd = number_of_arguments.times.map do
+        length = client.gets[1..-1].to_i
+        # length +2 because of \n and \r
+        client.read(length + 2).chomp
+      end
+
+      response = case cmd[0].downcase
+        when 'ping' then "+PONG\r\n"
+        when 'echo' then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
+        end
+
+
+      client.write response
     ensure
       client.close
     end
